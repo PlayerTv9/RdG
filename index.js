ffd2302b5c7e626440cb32213fccbfd69911a0a1
PlayerTv9 <99615526+PlayerTv9@users.noreply.github.com>
2023-01-02 21:02:08 +0100

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json());
 
 
 app.get('/',(req, res) =>{
-    res.sendFile(path.join(__dirname, '/index.html'));
+    res.sendFile(path.join(__dirname, '/home.html'));
 })
 app.get('/pitagora',(req, res) =>{
     res.sendFile(path.join(__dirname, '/static/pages/pitagora.html'));
